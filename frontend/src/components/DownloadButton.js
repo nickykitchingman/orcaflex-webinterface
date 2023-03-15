5101756462913da4632822909e6b5a7999042a07
Ianto Pettitt <Ianto.Pettitt@orcina.local>
2023-03-15 11:49:22 +0000

    broken stuff

--- frontend/src/components/DownloadButton.js
+++ frontend/src/components/DownloadButton.js
@@ -9,7 +9,7 @@ export default function DownloadButton(props) {
     const { promiseInProgress } = usePromiseTracker({area: props.area}); 
 
     if (promiseInProgress) {
-        return (<div  className="loader download-loading"><Oval width="100%" height="100%" color="var(--color-notice)" secondaryColor="var(--color-1-darker)" strokeWidth={5}/></div>);
+        return (<div className="loader download-loading"><Oval width="100%" height="100%" color="var(--color-notice)" secondaryColor="var(--color-1-darker)" strokeWidth={5}/></div>);
     }
     
     return (
