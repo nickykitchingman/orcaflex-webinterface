e071325d5cfc17b9ef7aee017ce9080a6402ca00
Ianto Pettitt <Ianto.Pettitt@orcina.local>
2023-03-16 09:58:05 +0000

    Add file extension to process button

--- frontend/src/components/ProcessButton.js
+++ frontend/src/components/ProcessButton.js
@@ -8,7 +8,7 @@ export default function DownloadButton(props) {
     
     return (
         <a className="process-btn" onClick={props.onClick}>
-            <div>{props.job.filename}</div>
+            <div id="brief-text">{props.job.filename} <div id="extension">{props.job.extension}</div></div>
             <div className="progress-message">{props.job.progress}</div>
             <div className="status-symbol" ><StatusSymbol symbol={props.job.status}/></div>
         </a>
